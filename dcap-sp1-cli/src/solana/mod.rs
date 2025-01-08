482e30bf9a2e6e15abe85e6771f9950cdc41ce6e
Preston Ong <preston4896@me.com>
2025-01-08 12:59:50 +0800

    modified sp1 solana program to take the hash of pubinput and added instruction to increase cu limits

--- dcap-sp1-cli/src/solana/mod.rs
+++ dcap-sp1-cli/src/solana/mod.rs
@@ -1,10 +1,13 @@
 use std::{env, fs};
 
+use sha2::{Digest, Sha256};
+
 use anyhow::Result;
 use dcap_sp1_solana_program::SP1Groth16Proof;
 use sp1_sdk::SP1ProofWithPublicValues;
 
 use solana_sdk::{
+    compute_budget::ComputeBudgetInstruction,
     instruction::{AccountMeta, Instruction},
     pubkey::Pubkey,
     signer::{keypair::Keypair, Signer},
@@ -13,28 +16,37 @@ use solana_sdk::{
 
 use solana_rpc_client::rpc_client::RpcClient;
 
-pub const PROGRAM_ID: &str = "4S1YicT8oGzj38a98bhiMdfNDrvLkdb5TB5Uw2tZ8qmq";
+pub const PROGRAM_ID: &str = "2LUaFQTJ7F96A5x1z5sXfbDPM2asGnrQ2hsE6zVDMhXZ";
 
 pub async fn run_verify_instruction(
-    vkey_hash: String,
     proof_with_public_values: SP1ProofWithPublicValues,
 ) -> Result<()> {
     // instantiate RPC client
-    let rpc_url = String::from("https://api.devnet.solana.com");
+    let rpc_url = std::env::var("SOLANA_RPC_URL")
+        .unwrap_or_else(|_| "https://api.devnet.solana.com".to_string());
+    println!("RPC URL: {}", rpc_url.as_str());
     let client = RpcClient::new(rpc_url);
 
     // instantiate payer
     let payer = load_payer()?;
-    println!("Payer address: {:?}", payer.pubkey().to_string());
+    println!("Payer address: {}", payer.pubkey().to_string());
+
+    // Compute the hash of the public inputs
+    let public_values_hash = Sha256::digest(proof_with_public_values.public_values.as_slice());
 
     // build the instruction data
     let groth16_proof = SP1Groth16Proof {
         proof: proof_with_public_values.bytes(),
-        sp1_public_inputs: proof_with_public_values.public_values.to_vec(),
-        vkey_hash,
+        sp1_public_inputs_hash: public_values_hash.to_vec(),
     };
     let program_id = Pubkey::from_str_const(PROGRAM_ID);
 
+    // Estimated Compute Unit needed = 300k CU
+    // Currently, the default limit is 200k CU
+    // Therefore, we need to include the SetComputeUnitLimit instruction
+    let estimated_compute_units: u32 = 300_000;
+    let set_compute_unit_limit_instruction =
+        ComputeBudgetInstruction::set_compute_unit_limit(estimated_compute_units);
     let instruction = Instruction::new_with_borsh(
         program_id,
         &groth16_proof,
@@ -42,10 +54,11 @@ pub async fn run_verify_instruction(
     );
 
     // Create and send transaction
-    let mut tx = Transaction::new_with_payer(&[instruction], Some(&payer.pubkey()));
+    let mut tx = Transaction::new_with_payer(
+        &[set_compute_unit_limit_instruction, instruction],
+        Some(&payer.pubkey()),
+    );
     tx.sign(&[&payer], client.get_latest_blockhash()?);
-
-    println!("data len: {}", tx.message_data().len());
 
     println!("Submitting instructions...");
     let signature = client.send_and_confirm_transaction(&tx)?;
